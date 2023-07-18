d12baaef8cef0059cb9329b4561b37c83d08063d
Mohamed Othman <mohamed.dev1991@gmail.com>
2023-07-18 15:10:21 +0300

    Update index.blade.php

--- src/Paysky/views/index.blade.php
+++ src/Paysky/views/index.blade.php
@@ -1,37 +1,45 @@
-    <button type="button" class="btn btn-info" id="paysky_payment_id" onclick="showLightBox()">Pay</button>
+<button type="button" class="btn btn-info" id="paysky_payment_id" onclick="showLightBox()">Pay</button>
+@isset($payment)
 
-@if (env('PaySky_Enviroment') == 'production')
-<script src="https://npg.moamalat.net:6006/js/lightbox.js"></script>
+    @if (env('PaySky_Enviroment') == 'production')
+        <script src="https://npg.moamalat.net:6006/js/lightbox.js"></script>
     @else
-<script src="https://tnpg.moamalat.net:6006/js/lightbox.js"></script>
+        <script src="https://tnpg.moamalat.net:6006/js/lightbox.js"></script>
+    @endif
 
-@endif
 
+    <script type="text/javascript">
+        function showLightBox() {
+            Lightbox.Checkout.configure = {
+                paymentMethodFromLightBox: "{{ $payment['paymentMethodFromLightBox'] }}",
+                MID: "{{ $payment['MID'] }}",
+                TID: "{{ $payment['TID'] }}",
+                AmountTrxn: "{{ $payment['AmountTrxn'] }}",
+                MerchantReference: "{{ $payment['MerchantReference'] }}",
+                TrxDateTime: "{{ $payment['TrxDateTime'] }}",
+                SecureHash: "{{ $payment['SecureHash'] }}",
+                completeCallback: function(data) {
+                    console.log('completed');
+                    console.log(data);
+                    var url = "{{ route('paysky.payment.completeCallback') }}";
+                    url += "?refNumber={{ $payment['MerchantReference'] }}";
+                    url += "&TxnNumber=" + data.SystemReference;
+                    window.location.href = url;
 
-<script type="text/javascript">
+                },
+                errorCallback: function(data) {
+                    console.log('error');
+                    console.log(data);
+                    var url = "{{ route('paysky.payment.errorCallback') }}";
+                    url += "?refNumber={{ $payment['MerchantReference'] }}";
+                    window.location.href = url;
+                },
+                cancelCallback: function() {
+                    console.log('cancel');
+                }
+            };
 
-    function showLightBox() {
-        Lightbox.Checkout.configure = {
-            paymentMethodFromLightBox: "{{ $payment['paymentMethodFromLightBox'] }}",
-            MID: "{{ $payment['MID'] }}",
-            TID: "{{ $payment['TID'] }}",
-            AmountTrxn: "{{ $payment['AmountTrxn'] }}",
-            MerchantReference: "{{ $payment['MerchantReference'] }}",
-            TrxDateTime: "{{ $payment['TrxDateTime'] }}",
-            SecureHash:  "{{ $payment['SecureHash'] }}" ,
-            completeCallback: function (data) {
-                console.log('completed');
-                console.log(data);
-            },
-            errorCallback: function (data) {
-                console.log('error');
-                console.log(data);
-            },
-            cancelCallback:function () {
-                console.log('cancel');
-            }
-        };
-
-        Lightbox.Checkout.showLightbox();
-    }
-</script>
+            Lightbox.Checkout.showLightbox();
+        }
+    </script>
+@endisset
